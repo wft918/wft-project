d4fff1a10057fb7d12282e72aa2ff03a5bdce976
王福太 <742834053@qq.com>
2022-04-13 18:01:14 +0800

    vue2实现vue3中的Teleport传送门，是西安B站视频播放效果

---
+++ src/views/pages/home/Teleport.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: true
+    },
+    disabled: {
+      type: Boolean,
+      default: true
+    }
+  },
+  watch: {
+    disabled() {
+      if(this.disabled) {
+        document.querySelector(this.to).appendChild(this.$el)
+      }
+    }
+  },
+
+  mounted(){
+    // console.log(this.to, '----->>>')
+    // console.log(document.querySelector(this.to), '----->>>')
+    if(this.disabled) document.querySelector(this.to).appendChild(this.$el)
+  },
+  /**
+   *  render方法中，如果有变量得使用{}
+   *  this.$scopedSlots.default() 表示预留一个插槽
+   */
+  render() {
+    return <div>{this.$scopedSlots.default()}</div>
+  }
+}
+</script>
--- src/views/pages/home/home.vue
+++ src/views/pages/home/home.vue
@@ -1,56 +1,74 @@
 <template>
-  <div class="home">
-        <div
-          v-for="(item,index) in 3"
-          :key="item"
-          :ref="'div' + index"
-          :data-index="index"
-          :id="'div' + index">
-          test{{item}}
+  <div id="main">
+    
+    <!-- 要播放的大视频窗口 -->
+    <div id="sourceBox">
+      <Teleport v-if="isShow" :disabled="isTeleport" :to="'#targetBox'">
+        <div>
+          <div>1234</div>
         </div>
+      </Teleport>
+    </div>
+
+    <div style="width: 100%;height: 2000px;"></div>
+
+    <!-- 被传送的小视频窗口 -->
+    <div id="targetBox"></div>
+
   </div>
 </template>
 <script>
+import Teleport from './Teleport.vue'
 export default {
   data() {
     return {
-      name:' '
+      isShow: false,
+      isTeleport: true,
+      // teleportNode: null
     }
   },
-  components: {
-    
-  },
-  mounted() {
-    // 这里通过id的方式去获取data- 正常获取
-    for(let i = 0; i < 3; i++) {
-      let ids = document.getElementById(`div${i}`)
-      console.log(ids.dataset)
-    }
-
-    /*
-     * 这里通过ref的方式来获取，出现一点点小问题
-     * 正常给一个 dom 元素绑定 ref ，我们通过this.$refs可以正常拿到dom元素；给组件绑定ref，我们可以通过this.$refs去调用组件的方法等等
-     * 但是这里，ref 和 v-for 一起使用的时候却一直拿不到，打印发现拿到是个数组，第一项才是我们要的dom元素，我也不知道为啥，就直接采用了下标的方式取的
-     * 
-     * 官方解释：当 ref 和 v-for 一起使用的时候，你得到的 ref 将会是一个包含了对应数据源的这些子组件的数组。
-     */
-    for(let i = 0; i < 3; i++) {
-      let divRefs = `div${i}`
-      console.log(this.$refs[divRefs][0].dataset)
-    }
+  components: { Teleport },
+  mounted(){
+    this.isShow = true
+    document.getElementById('main').addEventListener('scroll', this.intersectionObserver)
   },
   methods: {
-    butClick() {
-      
+    intersectionObserver() {
+      let main = document.getElementById('main')
+      let sourceBox = document.getElementById('sourceBox')
+      if(main.scrollTop >= sourceBox.getBoundingClientRect().height) {  // 传送
+        console.log('传-->>')
+        this.isTeleport = true
+      }else {  // 不传
+        this.isTeleport = false
+      }
     }
   }
 }
 </script>
-
 <style scoped>
-  .home {
+  #main {
     width: 100%;
     height: 100%;
-    background-color: #fff;
+    position: relative;
+    overflow: auto;
+  }
+  #sourceBox {
+    position: absolute;
+    width: 640px;
+    height: 320px;
+    left: 0;
+    top: 0;
+    border: 1px solid red;
+    box-sizing: border-box;
+  }
+  #targetBox {
+    position: fixed;
+    right: 20px;
+    bottom: 20px;
+    width: 320px;
+    height: 180px;
+    border: 1px solid red;
+    box-sizing: border-box;
   }
 </style>
