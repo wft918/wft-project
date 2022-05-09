b2b04493e80bb58f53374ecdfe55b3cbcdf2936f
王福太 <742834053@qq.com>
2022-05-09 18:03:22 +0800

    将二维码封装成组件

--- src/views/pages/practice/qrcode/index.vue
+++ src/views/pages/practice/qrcode/index.vue
@@ -1,36 +1,21 @@
 <template>
   <div style="width: 100%;height: 100%;">
-    <div id="qrcode" ref="qrcode"></div>
+    <QrCode :id="'QrCode'" :text="codeUrl" />
   </div>
 </template>
 <script>
-import QRCode from 'qrcodejs2'
+import QrCode from './qrCode.vue'
 export default {
   data() {
     return {
-      qrcode: ''
+      codeUrl: 'http://jindo.dev.naver.com/collie'  // 后端返回的二维码地址
     }
   },
+  components: { QrCode },
   mounted(){
-    this.creatQrCode()
+    setTimeout(() => {
+      this.codeUrl = 'http://naver.com'
+    }, 2000)
   },
-  methods: {
-    creatQrCode() {
-      // this.$refs.qrcode.innerHTML = ''; //清除二维码方法一
-      // this.qrcode.clear(); // 清除二维码方法二
-      let text = 'http://jindo.dev.naver.com/collie';  // 后端返回的二维码地址
-      this.qrcode = new QRCode(this.$refs.qrcode, {
-        text: text, //页面地址 ,如果页面需要参数传递请注意哈希模式#
-        width: '128', // 二维码宽度 （不支持100%）
-        height: '128', // 二维码高度 （不支持100%）
-        colorDark: '#000000',
-        colorLight: '#ffffff',
-        correctLevel: QRCode.CorrectLevel.H,
-      })
-      setTimeout(() => {
-        this.qrcode.makeCode("http://naver.com"); // make another code.
-      }, 3000)
-    }
-  }
 }
 </script>
