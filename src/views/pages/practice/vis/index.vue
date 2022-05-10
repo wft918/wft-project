8b754d043947181e166e56e6ca650acc5ced1377
wang1235905 <742834053@qq.com>
2022-05-10 20:29:31 +0800

    拓扑图

--- src/views/pages/practice/vis/index.vue
+++ src/views/pages/practice/vis/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="width: 100%;height: 100%;">
     <div class="vis-container">
-      <VisTopology :nodes="nodes" :id="'VisTopology'" />
+      <VisTopology :id="'VisTopology'" :nodes="nodes" />
     </div>
   </div>
 </template>
@@ -10,26 +10,20 @@ import VisTopology from './vis.vue'
 export default {
   data() {
     return {
-      nodes: [
-        { id: 1, label: "Node 1" },
-        { id: 2, label: "Node 2" },
-        { id: 3, label: "Node 3" },
-        { id: 4, label: "Node 4" },
-        { id: 5, label: "Node 5" },
-      ]
+      nodes: []
     }
   },
   components: { VisTopology },
   mounted() {
     setTimeout(() => {
       this.nodes = [
-        { id: 1, label: "wft 1" },
-        { id: 2, label: "wft 2" },
-        { id: 3, label: "wft 3" },
-        { id: 4, label: "wft 4" },
-        { id: 5, label: "wft 5" },
+        { id: 1, label: "Wft 1" },
+        { id: 2, label: "Wft 2" },
+        { id: 3, label: "Wft 3" },
+        { id: 4, label: "Wft 4" },
+        { id: 5, label: "Wft 5" }
       ]
-    }, 3000)
+    }, 2000)
   }
 }
 </script>
