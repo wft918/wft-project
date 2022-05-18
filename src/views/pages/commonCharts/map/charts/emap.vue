da8fbca4dc4e4a96f7739bdcd0c8aa12c8b9ea67
wang1235905 <742834053@qq.com>
2022-05-18 20:43:07 +0800

    地图上钻下钻

---
+++ src/views/pages/commonCharts/map/charts/emap.vue
@@ -0,0 +1,231 @@
+<template>
+  <div :id="id" style="width: 100%; height: 100%"></div>
+</template>
+<script>
+import { fontChart } from "@/utils/echartPxToRem";
+import { cityMap } from "@/utils/china-main-city-map.js";
+import * as echarts from "echarts";
+export default {
+  data() {
+    return {
+      charts: ""
+    };
+  },
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    cityName: {
+      type: String,
+      required: true
+    }
+  },
+  watch: {
+    cityName() {
+      this.LoadMap()
+    }
+  },
+  mounted() {
+    this.LoadMap();
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.selfAdaption);
+  },
+  methods: {
+    LoadMap() {
+      this.charts = echarts.init(document.getElementById(this.id));
+      const jsonCode = require("../../../../../assets/map/" + cityMap[this.cityName] + ".json");
+      echarts.registerMap(this.cityName, jsonCode);
+      let option = {
+        // backgroundColor: "black",
+        geo: {
+          map: this.cityName,
+          aspectScale: 0.8,
+          layoutCenter: ["50%", "50%"],
+          layoutSize: "120%",
+          itemStyle: {
+            areaColor: {
+              type: "linear-gradient",
+              x: 0,
+              y: 1200,
+              x2: 1000,
+              y2: 0,
+              colorStops: [
+                {
+                  offset: 0,
+                  color: "#152E6E", // 0% 处的颜色
+                },
+                {
+                  offset: 1,
+                  color: "#0673AD", // 50% 处的颜色
+                },
+              ],
+              global: true, // 缺省为 false
+            },
+            shadowColor: "#229ffc",
+            shadowOffsetX: -2,
+            shadowOffsetY: 8,
+            opacity: 0.5,
+          },
+          emphasis: {
+            label: { show: false },
+            areaColor: "red",
+          },
+          regions: [
+            {
+              name: "南海诸岛",
+              itemStyle: {
+                areaColor: "rgba(0, 10, 52, 1)",
+                borderColor: "rgba(0, 10, 52, 1)",
+                opacity: 0,
+                label: {
+                  show: false,
+                  color: "#009cc9",
+                },
+              },
+              label: {
+                show: false,
+                color: "#FFFFFF",
+                fontSize: 12,
+              },
+            },
+          ],
+        },
+        series: [
+          {
+            type: "map",
+            selectedMode: "multiple",
+            map: this.cityName,
+            aspectScale: 0.8,
+            layoutCenter: ["50%", "50%"], //地图位置
+            layoutSize: "120%",
+            zoom: 1.1, //当前视角的缩放比例
+            // roam: true, //是否开启平游或缩放
+            scaleLimit: {
+              //滚轮缩放的极限控制
+              min: 1,
+              max: 2,
+            },
+            label: {
+              show: false,
+              color: "#FFFFFF",
+              fontSize: 16,
+            },
+            itemStyle: {
+              areaColor: "#004b85",
+              borderColor: "#1cccff",
+              borderWidth: 1.8,
+            },
+            emphasis: {
+              //是图形在高亮状态下的样式,比如在鼠标悬浮或者图例联动高亮时
+              label: { 
+                show: true,
+                color: '#fff'
+              },
+              itemStyle: {
+                areaColor: "#198895",
+              },
+            },
+            data: [
+              {
+                name: "内蒙古",
+                itemStyle: {
+                  areaColor: "#56b1da",
+                },
+                value: [110.3467, 41.4899],
+              },
+            ],
+          },
+          // 添加散点 标记点
+          {
+            type: "scatter",
+            coordinateSystem: "geo",
+            symbol: "pin",
+            legendHoverLink: true,
+            symbolSize: [28, 28],
+            // legendHoverLink: true,
+            label: {
+              show: true,
+              offset: [0, -25], //偏移设置
+              fontSize: fontChart(15),
+              lineHeight: fontChart(13),
+              borderWidth: 1,
+              backgroundColor: "rgba(20, 43, 61, .9)",
+              borderColor: "#8f7136",
+              padding: fontChart(8),
+              formatter(value) {
+                // console.log(value,'value-->>');
+                // return value.data.name + '：' + value.data.value[2] + '人'
+                // return `{name|${value.data.name}}\nph值：{pt|${value.data.ph}}\n溶解氧：{pt|${value.data.rjy}}\n{date|更新时间：${value.data.upDate}}`;
+                return `{name|${value.data.name}}`
+              },
+              rich: {
+                name: {
+                  fontWeight: "bold",
+                  fontSize: fontChart(13),
+                  color: "#fff",
+                },
+                pt: {
+                  fontSize: 14,
+                  color: "#2BD8FB",
+                },
+                date: {
+                  color: "rgb(255,255,255,0.8)",
+                },
+              },
+              color: "#fff",
+            },
+            itemStyle: {
+              color: "#D8BC37", //标志颜色
+              shadowBlur: 2,
+              shadowColor: "D8BC37"
+            },
+            data: [  // 数据
+              {
+                name: '水质-露营基地',
+                value: [121.645189, 29.750277],
+                ph: 7.7,
+                rjy: 9.32,
+                upDate: '11-09 11:45',
+              },
+              {
+                name: '山东省',
+                value: [117.00, 36.40],
+                ph: 7.7,
+                rjy: 9.32,
+                upDate: '11-09 11:45',
+              },
+            ],
+            showEffectOn: "render",
+            rippleEffect: {
+              brushType: "stroke",
+            },
+            // hoverAnimation: true,
+            zlevel: 1,
+          }
+        ]
+      };
+      // 这里很重要！！解决重复点击
+      this.charts.off('click') 
+      this.charts.off('contextmenu')
+      this.charts.setOption(option, true);
+      // console.log(this.charts._chartsViews);  // ...
+      this.charts.on("click", (params) => { // 下钻
+        this.$emit('mapClick', params.name)
+      });
+      document.oncontextmenu = () => false  //去除默认的鼠标右击事件
+      this.charts.on('contextmenu', params => {
+        this.$emit('mapContextmenu')
+      })
+      window.addEventListener("resize", this.selfAdaption);
+    },
+    // 自适应
+    selfAdaption() {
+      if (!this.charts) return;
+      this.charts.resize();
+      this.LoadMap();
+    },
+  },
+};
+</script>
--- src/views/pages/commonCharts/map/china.vue
+++ src/views/pages/commonCharts/map/china.vue
@@ -6,25 +6,61 @@
     <div class="map map2">
       <ChinaMap2 :id="'ChinaMap2'" />
     </div>
-    <div class="map"></div>
+    <div class="map">
+      <div class="cityHierarchy">
+        <el-breadcrumb separator=">>">
+          <el-breadcrumb-item v-for="item in cityParams" :key="item" @click.native="toMap(item)">{{ item }}</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <div class="eMap">
+        <Emap :id="'Emap'" :cityName="cityName" @mapClick="nextMap" @mapContextmenu="backMap" />
+      </div>
+    </div>
     <div class="map"></div>
   </div>
 </template>
 <script>
 import ChinaMap1 from './charts/chinaChart1.vue'
 import ChinaMap2 from './charts/chinaChart2.vue'
+import Emap from './charts/emap.vue'
 export default {
   data() {
     return {
-
+      cityName: '全国',
+      cityParams: ['全国']
     }
   },
   components: {
     ChinaMap1,
-    ChinaMap2
+    ChinaMap2,
+    Emap
   },
   methods:{
-
+    //  地图下钻
+    nextMap(name) {
+      // 先获取数据，数据获取到之后再更新cityName 完成下钻
+      let toRegion = ['山东省', '济南市', '菏泽市']  // 这是可以进行下钻的地区
+      let lastRegion = ['郓城县', '鄄城县']  // 这是最后一层的地区
+      if(toRegion.indexOf(name) >= 0) {
+        this.cityName = name
+        this.cityParams.push(name)
+      }
+      if(lastRegion.indexOf(name) > 0) {// 这里注意，下钻到最后一级的时候就不要push了，这是为了上钻
+        // 点击最后一层时候做些什么事
+      }
+    },
+    // 地图上钻
+    backMap() {
+      if(this.cityParams.length > 1) {
+        this.cityName = this.cityParams.pop()
+      } else {
+        this.cityName = '全国'
+      }
+    },
+    // 点击上方面包屑
+    toMap(name) {
+      this.cityName = name
+    }
   },
 }
 </script>
@@ -48,5 +84,13 @@ export default {
 }
 .map2 {
   background: linear-gradient(to bottom , #1140ac, #0e2766, #0c2558);
+}
+.cityHierarchy {
+  width: 100%;
+  height: 35px;
+}
+.eMap {
+  width: 100%;
+  height: calc(100% - 35px);
 }
 </style>
