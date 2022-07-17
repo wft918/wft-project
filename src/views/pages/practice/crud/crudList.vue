69836d8c37875323eb235c3d577370a8284e8b37
wang1235905 <742834053@qq.com>
2022-07-17 14:02:48 +0800

    前端导出表格excel

--- src/views/pages/practice/crud/crudList.vue
+++ src/views/pages/practice/crud/crudList.vue
@@ -21,14 +21,16 @@
         <el-button type="warning" size="small" icon="el-icon-edit-outline" @click="edit()" :disabled="dataListSelections.length != 1" plain>修改</el-button>
         <el-button type="danger" size="small" icon="el-icon-delete" @click="del()" :disabled="dataListSelections.length <= 0" plain>删除
         </el-button>
+        <el-button icon="el-icon-download" @click="downloadTpl()" size="small">导出</el-button>
       </el-form-item>
     </el-form>
     <!-- 表格数据 -->
-    <el-table 
+    <el-table
+      id="myTable"
       :data="dataList"
-      size="medium" 
-      border 
-      @selection-change="selectionChangeHandle" 
+      size="medium"
+      border
+      @selection-change="selectionChangeHandle"
       v-loading="loading"
       class="table"
     >
@@ -109,78 +111,131 @@
   </div>
 </template>
 <script>
-  import crudForm from './crudForm'
-  export default {
-    data() {
-      return {
-        searchForm: {
-          name: '',//姓名
-          code: '',//编号
-          age: '',//年龄
-        },
-        dataListSelections:[],//多选
-        pageNo: 1,
-        pageSize: 10,
-        total: 0,
-        dataList: [],
-        loading: false,
-      }
+import crudForm from './crudForm'
+import * as XLSX from 'xlsx'
+import FileSaver from 'file-saver'
+export default {
+  data() {
+    return {
+      searchForm: {
+        name: '',//姓名
+        code: '',//编号
+        age: '',//年龄
+      },
+      dataListSelections:[],//多选
+      pageNo: 1,
+      pageSize: 10,
+      total: 0,
+      dataList: [
+        {
+          name: 'wft',
+          sex: '男',
+          code: '1212112',
+          height: '1.88',
+          shape: '偏瘦',
+          remarks: '备注123'
+        }
+      ],
+      loading: false,
+    }
+  },
+  components: {
+    crudForm
+  },
+  mounted() {
+    this.refreshList()
+  },
+  methods: {
+    // 获取列表数据
+    refreshList() {
+
     },
-    components: {
-      crudForm
-    },
-    mounted() {
+    // 重置
+    resetSearch() {
+      this.$refs.searchForm.resetFields()
       this.refreshList()
     },
-    methods: {
-      // 获取列表数据
-      refreshList() {
+    // 多选
+    selectionChangeHandle(val) {
 
-      },
-      // 重置
-      resetSearch() {
-        this.$refs.searchForm.resetFields()
-        this.refreshList()
-      },
-      // 多选
-      selectionChangeHandle(val) {
-
-      },
-      // 每页数
-      sizeChangeHandle(val) {
-        this.pageSize = val
-        this.pageNo = 1
-        this.refreshList()
-      },
-      // 当前页
-      currentChangeHandle(val) {
-        this.pageNo = val
-        this.refreshList()
-      },
-      // 新增
-      add() {
-        this.$refs.crudForm.init('add','')
-      },
-      // 修改
-      edit(id) {
-        id = id || this.dataListSelections.map(item => item.id)[0]
-        this.$refs.crudForm.init('edit',id)
-      },
-      // 查看
-      view(id) {
-        this.$refs.crudForm.init('view',id)
-      },
-      // 删除
-      del(id) {
-        let ids = id || this.dataListSelections.map(item => item.id).join(',')
-        this.$confirm(`确定删除所选项吗?`, '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          
-        })
+    },
+    // 每页数
+    sizeChangeHandle(val) {
+      this.pageSize = val
+      this.pageNo = 1
+      this.refreshList()
+    },
+    // 当前页
+    currentChangeHandle(val) {
+      this.pageNo = val
+      this.refreshList()
+    },
+    // 新增
+    add() {
+      this.$refs.crudForm.init('add','')
+    },
+    // 修改
+    edit(id) {
+      id = id || this.dataListSelections.map(item => item.id)[0]
+      this.$refs.crudForm.init('edit',id)
+    },
+    // 查看
+    view(id) {
+      this.$refs.crudForm.init('view',id)
+    },
+    // 删除
+    del(id) {
+      let ids = id || this.dataListSelections.map(item => item.id).join(',')
+      this.$confirm(`确定删除所选项吗?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        
+      })
+    },
+    // 导出
+    downloadTpl() {
+      let wb = XLSX.utils.table_to_book(document.getElementById('myTable'), { raw: true })
+      wb.Sheets.Sheet1 = this.delRepeat(wb.Sheets.Sheet1) // 去掉wb.Sheets.Sheet1对象中重复的value的属性
+      // console.log(wb, '----->>>')
+      let wbout = XLSX.write(wb, {
+        bookType: 'xlsx',
+        bookSST: true,
+        type: 'array'
+      })
+      // console.log(wbout, 'wbout----->>>')
+      try {
+        FileSaver.saveAs(
+          //Blob 对象表示一个不可变、原始数据的类文件对象。
+          //Blob 表示的不一定是JavaScript原生格式的数据。
+          //File 接口基于Blob，继承了 blob 的功能并将其扩展使其支持用户系统上的文件。
+          //返回一个新创建的 Blob 对象，其内容由参数中给定的数组串联组成。
+          new Blob([wbout], { type: "application/octet-stream" }),
+          //设置导出文件名称
+          "订单.xlsx"
+        );
+      } catch(e) {
+        console.error(e, wbout, '----->>>')
       }
+      // return wbout
+    },
+    // 对象去重
+    delRepeat(obj) {
+      let noRepeat = {}
+      for(let key in obj) {
+        let flag = true
+        for(let k in noRepeat) {
+          if(JSON.stringify(noRepeat[k]) == JSON.stringify(obj[key])) {
+            flag = false
+          }
+        }
+        if(flag) {
+          noRepeat[key] = obj[key]
+        }
+      }
+      return noRepeat
     }
   }
+}
 </script>
