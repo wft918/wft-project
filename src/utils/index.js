f0c06368b146e2f6cbe92d9a99220d3c735f2285
王福太 <742834053@qq.com>
2022-08-01 09:54:56 +0800

    增加工具类方法

--- src/utils/index.js
+++ src/utils/index.js
@@ -14,6 +14,51 @@ export function deepClone(sourceObj) {
   return targetObj
 }
 
+export class SessionUtils {
+  static get(key) {
+    return JSON.parse(window.sessionStorage.getItem(key))
+  }
+  static set(key, value) {
+    window.sessionStorage.setItem(key, JSON.stringify(value))
+  }
+  static remove(key) {
+    window.sessionStorage.removeItem(key)
+  }
+}
 
+export class FindArrsById {
+  constructor(id, tree) {
+    this.id = id
+    this.flatArr = this.flatTreeAndSetLevel.call(this, tree)
+    this.parentAreas = this.getParentAreas.call(this, this.id, this.flatArr)
+    this.getParentValuesByKey.bind(this)
+  }
+
+  flatTreeAndSetLevel(tree, level = 0) {
+    const list = []
+    tree.forEach(item => {
+      const o = JSON.parse(JSON.stringify(item))
+      if(o.children) delete o.children
+      o.level = level
+      list.push(o)
+      if(item.children && item.children.length) {
+        list.push(...this.flatTree(item.children, level + 1))
+      }
+    })
+    return list
+  }
+
+  getParentAreas(pid, list) {
+    const target = []
+    const o = list.find(item => item.id == pid) || {}
+    if(JSON.stringify(o) !== '{}') target.push(o)
+    if(o.parentId) target.push(...this.getParentAreas(o.parentId, list))
+    return target
+  }
+
+  getParentValuesByKey(key) {
+    return this.parentAreas.map(item => item[key]).reverse()
+  }
+}
 
 export default { deepClone }
